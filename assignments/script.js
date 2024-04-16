3333b007ba9cf3547a2239854efaaac0e0eae19a
kylie pele <criispy@berkeley.edu>
2024-04-15 20:47:13 -0700

    lab8

--- assignments/script.js
+++ assignments/script.js
@@ -1,6 +1,6 @@
 /* UPDATE FOR EVERY ASSIGNMENT: */
 
-const assignments = ['lab1', 'hw1', 'lab2', 'hw2', 'lab3', 'hw3', 'hw4', 'midterm', 'hw5', 'lab4', 'lab5', 'hw6', 'hw7', 'hw8', 'final', 'lab7', 'hw9'];
+const assignments = ['lab1', 'hw1', 'lab2', 'hw2', 'lab3', 'hw3', 'hw4', 'midterm', 'hw5', 'lab4', 'lab5', 'hw6', 'hw7', 'hw8', 'final', 'lab7', 'hw9', 'lab8'];
 
 
 const template = document.getElementById('assignment-template');
